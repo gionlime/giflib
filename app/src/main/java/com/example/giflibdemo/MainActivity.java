45dd772f1ed645ea5cec9dbe2699030de7292848
lijunjun <gionchat@gmail.com>
2022-04-07 09:14:51 +0800

    1、添加gif 网站

--- app/src/main/java/com/example/giflibdemo/MainActivity.java
+++ app/src/main/java/com/example/giflibdemo/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.iv);
-
+        //gif网  https://www.vcg.com/sets/516794080
         String gif = "https://tenfei03.cfp.cn/creative/vcg/nowater800/new/VCG211291391697.gif";
         Glide.with(this).as(FrameSequenceDrawable.class).load(gif).into(imageView);
     }

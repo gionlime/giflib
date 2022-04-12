22185aa42245f9cd32a92bb8a9d19d403b1d319d
lijunjun <gionchat@gmail.com>
2022-04-12 11:31:32 +0800

    1、加载本地gif图片

--- app/src/main/java/com/example/giflibdemo/MainActivity.java
+++ app/src/main/java/com/example/giflibdemo/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.iv);
         //gif网  https://www.vcg.com/sets/516794080
         String gif = "https://alifei05.cfp.cn/creative/vcg/nowater800/new/VCG211291463845.gif";
-        Glide.with(this).as(FrameSequenceDrawable.class).load(gif).into(imageView);
+        // TODO: 加载网络gif
+        //Glide.with(this).as(FrameSequenceDrawable.class).load(gif).into(imageView);
+        // TODO: 加载本地gif图片
+        Glide.with(this).as(FrameSequenceDrawable.class).load(R.raw.drink_to).into(imageView);
     }
 }

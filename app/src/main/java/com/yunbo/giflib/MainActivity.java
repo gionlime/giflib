109fa200095b72f0ddca01668f71ad9ac5d021ad
lijunjun <gionchat@gmail.com>
2022-05-16 17:52:36 +0800

    1、aar打包命令： ./gradlew app:assembleRelease

--- app/src/main/java/com/yunbo/giflib/MainActivity.java
+++ app/src/main/java/com/yunbo/giflib/MainActivity.java
@@ -3,10 +3,8 @@ package com.yunbo.giflib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.support.rastermill.FrameSequenceDrawable;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 /**
  * create by jeek
  * 2022/4/7
@@ -25,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         //gif网  https://www.vcg.com/sets/516794080
         String gif = "https://alifei05.cfp.cn/creative/vcg/nowater800/new/VCG211291463845.gif";
         // TODO: 加载网络gif
-        //Glide.with(this).as(FrameSequenceDrawable.class).load(gif).into(imageView);
+        GifUtils.loadGifByNetwork(MainActivity.this,gif,imageView);
         // TODO: 加载本地gif图片
-        Glide.with(this).as(FrameSequenceDrawable.class).load(R.raw.drink_to).into(imageView);
+//        GifUtils.loadGifByResId(MainActivity.this,R.raw.drink_to,imageView);
     }
 }

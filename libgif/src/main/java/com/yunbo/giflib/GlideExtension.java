1e5e01d21210535ce7ab1bc0d6802ff9ad8d55f2
lijunjun <gionchat@gmail.com>
2022-05-17 10:38:53 +0800

    1、修改moudle名字

---
+++ libgif/src/main/java/com/yunbo/giflib/GlideExtension.java
@@ -0,0 +1,28 @@
+package com.yunbo.giflib;
+
+import android.support.rastermill.FrameSequenceDrawable;
+
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.annotation.GlideType;
+import com.bumptech.glide.request.RequestOptions;
+/**
+ * create by jeek
+ * 2022/4/7
+ * des:
+ **/
+@com.bumptech.glide.annotation.GlideExtension
+public class GlideExtension {
+
+    private GlideExtension() {
+
+    }
+
+    final static RequestOptions DECODE_TYPE = RequestOptions
+            .decodeTypeOf(FrameSequenceDrawable.class)
+            .lock();
+
+    @GlideType(FrameSequenceDrawable.class)
+    public static void asGif2(RequestBuilder<FrameSequenceDrawable> requestBuilder) {
+        requestBuilder.apply(DECODE_TYPE);
+    }
+}
